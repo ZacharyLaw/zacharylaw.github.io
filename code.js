5fac49c721201bf3681f83932576f32dc36af02f
ZacharyLawPep <zacharylaw@peplink.com>
2024-01-26 13:15:29 +0800

    git dev

--- code.js
+++ code.js
@@ -1,6 +1,9 @@
-function doGet() {
+function doGet(req) {
   console.log(req)
-  return html('main')
+  console.log('how are you not saying hai')
+  Logger.log('prod no log?')
+  if(req.parameter.page=='map') return html('map')
+  else return html('main')
 }
 function html(filename){
   return HtmlService.createTemplateFromFile(filename).evaluate().addMetaTag('viewport','width=device-width,initial-scale=1').setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
